396d0f2a8e995425b4051699fcdaec5288a2f29f
HinaTharwani <113449700+hinatharwani@users.noreply.github.com>
2022-11-06 14:47:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
+class Shape{
+  constructor(x,y) {
+  this.xPosition = x;
+  this.yPosition - y;
+  }
+  getArea() {...}
+}
+  class Circle extend Shape {
+    constructor(x, y, radius) {
+      super(x, y , radius);
+      this.radius = radius
+    }
+      getArea() {...}
+   }
+    let circle = new Circle(1,2,3):
 ReactDOM.render(<App />, document.getElementById("root"));
